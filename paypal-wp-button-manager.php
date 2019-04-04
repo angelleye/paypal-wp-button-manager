d4882c54d93b57f4f35115e045ed08a835dd2d75
kcppdevelopers <kcppdevelopers@gmail.com>
2019-04-04 12:01:48 +0530

    AE Updater plugin link, PBM-29

--- paypal-wp-button-manager.php
+++ paypal-wp-button-manager.php
@@ -44,7 +44,7 @@ if (!defined('PAYPAL_WP_BUTTON_MANAGER_PLUGIN_BASENAME')) {
 }
 
 if (!defined('AEU_ZIP_URL')) {
-    define('AEU_ZIP_URL', 'https://github.com/angelleye/angelleye-updater/archive/master.zip');
+    define('AEU_ZIP_URL', 'https://updates.angelleye.com/ae-updater/angelleye-updater/angelleye-updater.zip');
 }
 
 /**
