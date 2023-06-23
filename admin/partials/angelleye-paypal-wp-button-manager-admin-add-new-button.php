960e5125b9c31ed25be97e3d58ee97b6cd5e4c7d
Jaydeep Rajgor <jaydeep.rajgor@quanticedge.co.in>
2023-06-23 11:43:08 +0530

    Updated add new page

--- admin/partials/angelleye-paypal-wp-button-manager-admin-add-new-button.php
+++ admin/partials/angelleye-paypal-wp-button-manager-admin-add-new-button.php
@@ -4,7 +4,7 @@
             <div class="paypal-wp-l-logo-details">
                 <img width="159" height="188" src="<?php echo ANGELLEYE_PAYPAL_WP_BUTTON_MANAGER_IMAGE_PATH . 'logo.png'; ?>">
                 <div class="paypal-wp-l-details">
-                	<a class="paypal-add-company button" href="<?php echo admin_url('admin.php?page=' . self::$paypal_button_company_slug . '&type=new'); ?>"><?php _e('Add Company','angelleye-paypal-wp-button-manager'); ?></a>
+                	<a class="paypal-add-company button" href="<?php echo admin_url('admin.php?page=' . self::$paypal_button_company_slug . '&type=new'); ?>"><?php _e('Add Account','angelleye-paypal-wp-button-manager'); ?></a>
                 </div>
             </div>
          	<div class="wave-vector-wordpress">
