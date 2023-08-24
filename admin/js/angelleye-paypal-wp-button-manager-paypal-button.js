846cf62ca52fd583cea72bd50f27538550b68908
Jaydeep Rajgor <jaydeep.rajgor@quanticedge.co.in>
2023-08-24 18:50:26 +0530

    PBM-70 Resolved draft button not showing preview

--- admin/js/angelleye-paypal-wp-button-manager-paypal-button.js
+++ admin/js/angelleye-paypal-wp-button-manager-paypal-button.js
@@ -16,6 +16,7 @@ function angelleyeUpdateConfig(){
 }
 
 jQuery(document).on('change', '.wbp-field, #wbp-button-hide-funding', angelleyeUpdateConfig);
+jQuery(document).on('change', '#item-name, #item-price, #item_price_currency, .shipping-tax.row .shipping-amount, .text-rate, #hide-data-fields', previewData);
 
 jQuery(function($){
     $("#wbp-button-layout").trigger('change');
@@ -23,6 +24,8 @@ jQuery(function($){
     $("#wbp-button-hide-funding").select2({
         placeholder: wbp_select2.placeholder
     });
+
+    previewData();
 
     $(".paypal_shortcode_copy").on('click',function(){
         var temp = $("<input>");
@@ -110,7 +113,9 @@ jQuery(function($){
         }
     });
 
-    $('.angelleye-color-picker').wpColorPicker();
+    $('.angelleye-color-picker').wpColorPicker({
+        change: previewData
+    });
 });
 
 jQuery(document).on('change','#wbp-button-layout',function(){
@@ -154,4 +159,83 @@ function hideDonateFields(){
 
 function showDonateFields(){
     jQuery('#donate_group').show();
+}
+
+function priceHTML(amount){
+    return '<span class="wbp-price"><span class="currency">' + jQuery('#item_price_currency').find(":selected").data('title') + '</span><span class="amount">' + parseFloat(amount).toFixed(2) + '</span></span>';
+}
+
+function previewData(){
+    jQuery(".wbp-form").hide();
+
+    if( jQuery("#item-name").val() ){
+        jQuery(".item-name").text(jQuery("#item-name").val());
+        jQuery(".item-name-details").show();
+    } else {
+        jQuery(".item-name-details").hide();
+    }
+
+    var price = 0;
+    if( jQuery("#item-price").val() ){
+        jQuery(".item-price").html( priceHTML( jQuery("#item-price").val() ) );
+        price += parseFloat( jQuery("#item-price").val() );
+        jQuery(".price-currency").show();
+    } else {
+        jQuery(".price-currency").hide();
+    }
+
+    if( jQuery(".shipping-amount").val() ){
+        jQuery(".shipping-rate .shipping").html( priceHTML( jQuery(".shipping-amount").val() ) );
+        price += parseFloat( jQuery(".shipping-amount").val() );
+        jQuery(".shipping-rate").show();
+    } else {
+        jQuery(".shipping-rate").hide();
+    }
+
+    var tax = 0;
+    if( jQuery(".text-rate").val() ){
+        jQuery(".tax-rate-label").text( angelleye_paypal_wp_button_manager_admin_paypal_button.preview_tax_label.replace('%s', jQuery(".text-rate").val() ) );
+        tax = parseFloat( jQuery(".text-rate").val() );
+        var tax_price = ( price * ( tax / 100 ) );
+        jQuery(".tax-amount").html( priceHTML( tax_price ) );
+        jQuery(".tax-rate").show();
+    } else {
+        jQuery(".tax-rate").hide();
+    }
+
+    if( price != 0 ){
+        if( tax != 0 ){
+            var total_price = price + ( price * ( tax / 100 ) );
+        } else {
+            var total_price = price;
+        }
+        jQuery(".total-amount").html( priceHTML( total_price ) );
+        jQuery(".total-amount-label").parent().show();
+    } else {
+        jQuery(".total-amount-label").parent().hide();
+    }
+
+    if( jQuery('input[name="left_background_color"]').val() ){
+        var left_background_color = jQuery('input[name="left_background_color"]').val();
+        jQuery('.item-name-label, .item-price-label, .shipping-rate-label, .tax-rate-label, .total-amount-label').css('background-color', left_background_color);
+    }
+
+    if( jQuery('input[name="left_foreground_color"]').val() ){
+        var left_foreground_color = jQuery('input[name="left_foreground_color"]').val();
+        jQuery('.item-name-label, .item-price-label, .shipping-rate-label, .tax-rate-label, .total-amount-label').css('color', left_foreground_color);
+    }
+
+    if( jQuery('input[name="right_background_color"]').val() ){
+        var right_background_color = jQuery('input[name="right_background_color"]').val();
+        jQuery('.item-name, .item-price, .shipping-rate .shipping, .tax-amount, .total-amount').css('background-color', right_background_color);
+    }
+
+    if( jQuery('input[name="right_foreground_color"]').val() ){
+        var right_foreground_color = jQuery('input[name="right_foreground_color"]').val();
+        jQuery('.item-name, .item-price, .shipping-rate .shipping, .tax-amount, .total-amount').css('color', right_foreground_color);
+    }
+
+    if( !jQuery("#hide-data-fields").is(':checked') ){
+        jQuery(".wbp-form").show();
+    }
 }
